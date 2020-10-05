def43219b41302801289fe438d105a1eef1b3754
Michal Ajduk <michal.ajduk@canonical.com>
2020-10-05 15:14:58 +0200

    Add snapshot feature

--- src/charm.py
+++ src/charm.py
@@ -12,6 +12,8 @@ from ops.model import ActiveStatus, BlockedStatus
 import subprocess
 import os
 import time
+from datetime import datetime
+import shutil
 
 logger = logging.getLogger(__name__)
 
@@ -25,14 +27,24 @@ class SimpleStreamsCharm(CharmBase):
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.update_status, self._on_update_status)
         self.framework.observe(self.on.synchronize_action, self._on_synchronize_action)
+        self.framework.observe(self.on.create_snapshot_action, self._on_create_snapshot_action)
+        self.framework.observe(self.on.publish_snapshot_action, self._on_publish_snapshot_action)
+        self.framework.observe(self.on.list_snapshots_action, self._on_list_snapshots_action)
+        self.framework.observe(self.on.delete_snapshot_action, self._on_delete_snapshot_action)
+        self.framework.observe(self.on.publish_relation_joined,
+                               self._on_publish_relation_joined)
         self._stored.set_default(config={})
 
+    def _on_publish_relation_joined(self, event):
+        publish_path = self._image_publish_dir()
+        event.relation.data[self.model.unit].update({'path': publish_path})
+
     def _on_update_status(self, _):
-        path = self._stored.config['image-dir'] + '/.data'
+        path = self._image_publish_dir() + '/.data'
         if os.path.isdir(path):
             stat = os.stat(path)
             self.model.unit.status = \
-                ActiveStatus("Last sync: {}".format(time.ctime(stat.st_mtime)))
+                ActiveStatus("Publishes: {}".format(time.ctime(stat.st_mtime)))
         else:
             self.model.unit.status = BlockedStatus("Images not synchronized")
 
@@ -48,6 +60,10 @@ class SimpleStreamsCharm(CharmBase):
                 value = self.model.config[key]
                 logger.info("Setting {} to: {}".format(key, value))
                 self._stored.config[key] = value
+        if not os.path.isdir(self._image_download_dir()):
+            os.makedirs(self._image_download_dir())
+        if not os.path.isdir(self._image_publish_dir()):
+            os.symlink(self._image_download_dir(), self._image_publish_dir())
         if 'cron-schedule' in self._stored.config and \
            self._stored.config['cron-schedule'] != 'None':
             self._setup_cron_job(self._stored.config)
@@ -64,10 +80,57 @@ class SimpleStreamsCharm(CharmBase):
                 event.fail(e.output)
                 return
 
+    def _on_create_snapshot_action(self, event):
+        snapshot_name = self._get_snapshot_name()
+        logger.info("Create snapshot {}".format(snapshot_name))
+        snapshot_root = "{}/{}".format(self._stored.config['image-dir'], snapshot_name)
+        download_root = self._image_download_dir()
+        if not os.path.exists(snapshot_root):
+            os.makedirs(snapshot_root)
+        for product in next(os.walk(download_root))[1]:
+            if '.data' != product:
+                if self._stored.config['copy-on-snapshot']:
+                    shutil.copytree("{}/{}".format(download_root, product),
+                                    "{}/{}".format(snapshot_root, product))
+                else:
+                    os.symlink("{}/{}".format(download_root, product),
+                               "{}/{}".format(snapshot_root, product))
+        shutil.copytree("{}/.data".format(download_root),
+                        "{}/.data".format(snapshot_root))
+
+    def _on_delete_snapshot_action(self, event):
+        snapshot = event.params["name"]
+        logger.info("Delete snapshot {}".format(snapshot))
+        shutil.rmtree("{}/{}".format(self._stored.config['image-dir'], snapshot))
+
+    def _on_list_snapshots_action(self, event):
+        snapshots = []
+        for directory in next(os.walk("{}/".format(self._stored.config['image-dir'])))[1]:
+            if directory.startswith("snapshot-"):
+                snapshots.append(directory)
+        logger.info("List snapshots {}".format(snapshots))
+        event.set_results({"snapshots": snapshots})
+
+    def _on_publish_snapshot_action(self, event):
+        name = event.params["name"]
+        logger.info("Publish snapshot {}".format(name))
+        snapshot_path = "{}/{}".format(self._stored.config['image-dir'], name)
+        publish_path = self._image_publish_dir()
+        if not os.path.isdir(snapshot_path):
+            event.fail("Snapshot does not exist")
+            return
+        if os.path.islink(publish_path):
+            os.unlink(publish_path)
+        os.symlink(snapshot_path, publish_path)
+        event.set_results({name: publish_path})
+
     def _sync_selector_cmd(self, selector, esc_char=None):
         cmd = [
-            'sstream-mirror'
+            'sstream-mirror',
         ]
+        if 'keep' not in self._stored.config or\
+           self._stored.config['keep']:
+            cmd.append("--keep")
         if 'keyring-file' not in self._stored.config or\
            not self._stored.config['keyring-file']:
             cmd.append("--no-verify")
@@ -82,7 +145,7 @@ class SimpleStreamsCharm(CharmBase):
         if self._stored.config['image-max']:
             cmd.append('--max={}'.format(self._stored.config['image-max']))
         cmd.append(self._stored.config['image-source'])
-        cmd.append(self._stored.config['image-dir'])
+        cmd.append(self._image_download_dir())
         for s in selector.split():
             if esc_char:
                 cmd.append("{}{}{}".format(esc_char, s, esc_char))
@@ -99,6 +162,15 @@ class SimpleStreamsCharm(CharmBase):
                         " ".join(self._sync_selector_cmd(selector, '\''))
                     )
                 )
+
+    def _image_download_dir(self):
+        return "{}/latest".format(self._stored.config['image-dir'])
+
+    def _image_publish_dir(self):
+        return "{}/publish".format(self._stored.config['image-dir'])
+
+    def _get_snapshot_name(self):
+        return 'snapshot-{}'.format(datetime.now().strftime("%Y%m%d%H%M%S"))
 
 
 if __name__ == "__main__":
--- tests/test_charm.py
+++ tests/test_charm.py
@@ -28,6 +28,8 @@ class TestCharm(unittest.TestCase):
             'image-source': str(uuid4()),
             'image-dir': str(uuid4()),
             'cron-schedule': 'None',
+            'copy-on-snapshot': False,
+            'keep': True,
         }
 
     @patch('subprocess.check_output')
@@ -41,17 +43,27 @@ class TestCharm(unittest.TestCase):
         self.assertTrue(mock_subproc.called)
         assert mock_subproc.call_args == call(["apt", "install", "-y", "simplestreams"])
 
-    def test_config_changed(self):
+    @patch('os.symlink')
+    @patch('os.makedirs')
+    @patch('os.path.isdir')
+    def test_config_changed(self, os_path_isdir, os_makedirs, os_symlink):
         harness = Harness(SimpleStreamsCharm)
         self.addCleanup(harness.cleanup)
+        os_path_isdir.return_value = False
         harness.begin()
         default_config = self.default_config()
         self.assertEqual(harness.charm._stored.config, {})
         harness.update_config(default_config)
+        self.assertTrue(os_path_isdir.called)
+        self.assertTrue(os_makedirs.called)
+        self.assertTrue(os_symlink.called)
         assert harness.charm._stored.config == default_config
 
+    @patch('os.symlink')
+    @patch('os.makedirs')
+    @patch('os.path.isdir')
     @patch('builtins.open', new_callable=mock_open)
-    def test_cron_schedule_set(self, mock_open_call):
+    def test_cron_schedule_set(self, mock_open_call, os_path_isdir, os_makedirs, os_symlink):
         harness = Harness(SimpleStreamsCharm)
         self.addCleanup(harness.cleanup)
         harness.begin()
@@ -63,7 +75,7 @@ class TestCharm(unittest.TestCase):
         mock_open_call.assert_called_with('/etc/cron.d/{}'.format(harness.charm.model.app.name),
                                           "w")
         mock_open_call.return_value.write.assert_called_once_with(
-            "{} root sstream-mirror"
+            "{} root sstream-mirror --keep"
             " --keyring={} --path={} --log-file={} --max={} {} {} \'{}\'\n"
             .format(
                 default_config['cron-schedule'],
@@ -72,10 +84,13 @@ class TestCharm(unittest.TestCase):
                 default_config['log-file'],
                 default_config['image-max'],
                 default_config['image-source'],
-                default_config['image-dir'],
+                "{}/latest".format(default_config['image-dir']),
                 default_config['image-selectors'],
             )
         )
+        self.assertTrue(os_path_isdir.called)
+        self.assertFalse(os_makedirs.called)
+        self.assertFalse(os_symlink.called)
 
     @patch('subprocess.check_output')
     def test_synchronize_action_defaults(self, mock_subproc):
@@ -90,15 +105,16 @@ class TestCharm(unittest.TestCase):
             'image-source': str(uuid4()),
             'image-dir': str(uuid4()),
             'cron-schedule': 'None',
+            'copy-on-snapshot': False
         }
         harness.charm._stored.config = minimal_config
         action_event = Mock()
         harness.charm._on_synchronize_action(action_event)
         self.assertTrue(mock_subproc.called)
-        assert mock_subproc.call_args == call(['sstream-mirror', '--no-verify', '--max={}'
-                                               .format(minimal_config['image-max']),
+        assert mock_subproc.call_args == call(['sstream-mirror', '--keep', '--no-verify',
+                                               '--max={}'.format(minimal_config['image-max']),
                                                minimal_config['image-source'],
-                                               minimal_config['image-dir'],
+                                               "{}/latest".format(minimal_config['image-dir']),
                                                minimal_config['image-selectors']],
                                               stderr=-2)
 
@@ -114,7 +130,7 @@ class TestCharm(unittest.TestCase):
         action_event = Mock()
         harness.charm._on_synchronize_action(action_event)
         self.assertTrue(mock_subproc.called)
-        assert mock_subproc.call_args == call(['sstream-mirror',
+        assert mock_subproc.call_args == call(['sstream-mirror', '--keep',
                                                '--keyring={}'
                                                .format(default_config['keyring-file']),
                                                '--verbose',
@@ -122,6 +138,153 @@ class TestCharm(unittest.TestCase):
                                                '--log-file={}'.format(default_config['log-file']),
                                                '--max={}'.format(default_config['image-max']),
                                                default_config['image-source'],
-                                               default_config['image-dir'],
+                                               "{}/latest".format(default_config['image-dir']),
                                                default_config['image-selectors']],
                                               stderr=-2)
+
+    @patch('os.symlink')
+    @patch('os.makedirs')
+    @patch('os.path.isdir')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_publish_relation_joined(self, mock_open_call, os_path_isdir, os_makedirs, os_symlink):
+        harness = Harness(SimpleStreamsCharm)
+        harness.begin()
+        default_config = self.default_config()
+        self.assertEqual(harness.charm._stored.config, {})
+        harness.update_config(default_config)
+        relation_id = harness.add_relation('publish', 'webserver')
+        harness.add_relation_unit(relation_id, 'webserver/0')
+        assert harness.get_relation_data(relation_id, harness._unit_name)\
+               == {'path': '{}/publish'.format(default_config['image-dir'])}
+
+    @patch('os.walk')
+    @patch('shutil.copytree')
+    @patch('os.path.exists')
+    @patch('os.symlink')
+    @patch('os.makedirs')
+    def test_create_snapshot_action(self, os_makedirs, os_symlink, os_path_exists,
+                                    shutil_copytree, os_walk):
+        def a2g(x):
+            return ([n, ['{}'.format(n)]] for n in x)
+
+        rand_n = random.randint(10, 100)
+        os_walk.side_effect = iter([a2g([rand_n])])
+        os_path_exists.return_value = False
+        default_config = self.default_config()
+        harness = Harness(SimpleStreamsCharm)
+        harness.begin()
+        harness.charm._stored.config = default_config
+        harness.charm._get_snapshot_name = Mock()
+        snapshot_name = uuid4()
+        harness.charm._get_snapshot_name.return_value = snapshot_name
+        action_event = Mock()
+        harness.charm._on_create_snapshot_action(action_event)
+        self.assertTrue(os_symlink.called)
+        assert os_symlink.call_args == call('{}/latest/{}'
+                                            .format(default_config['image-dir'], rand_n),
+                                            '{}/{}/{}'.format(default_config['image-dir'],
+                                                              snapshot_name, rand_n))
+        assert shutil_copytree.call_args == call('{}/latest/.data'
+                                                 .format(default_config['image-dir']),
+                                                 '{}/{}/.data'.format(default_config['image-dir'],
+                                                                      snapshot_name))
+
+    @patch('os.walk')
+    @patch('shutil.copytree')
+    @patch('os.path.exists')
+    @patch('os.makedirs')
+    def test_create_snapshot_copy_action(self, os_makedirs, os_path_exists,
+                                         shutil_copytree, os_walk):
+        def a2g(x):
+            return ([n, ['{}'.format(n)]] for n in x)
+
+        rand_n = random.randint(10, 100)
+        os_walk.side_effect = iter([a2g([rand_n])])
+        os_path_exists.return_value = False
+        default_config = self.default_config()
+        default_config['copy-on-snapshot'] = True
+        harness = Harness(SimpleStreamsCharm)
+        harness.begin()
+        harness.charm._stored.config = default_config
+        harness.charm._get_snapshot_name = Mock()
+        snapshot_name = uuid4()
+        harness.charm._get_snapshot_name.return_value = snapshot_name
+        action_event = Mock()
+        harness.charm._on_create_snapshot_action(action_event)
+        assert shutil_copytree.call_args_list[0] == call('{}/latest/{}'
+                                                         .format(default_config['image-dir'],
+                                                                 rand_n),
+                                                         '{}/{}/{}'
+                                                         .format(default_config['image-dir'],
+                                                                 snapshot_name,
+                                                                 rand_n))
+        assert shutil_copytree.call_args_list[1] == call('{}/latest/.data'
+                                                         .format(default_config['image-dir']),
+                                                         '{}/{}/.data'
+                                                         .format(default_config['image-dir'],
+                                                                 snapshot_name))
+
+    @patch('os.walk')
+    def test_list_snapshots_action(self, os_walk):
+        def a2g(x):
+            return ([n, ['snapshot-{}'.format(n)]] for n in x)
+
+        rand_n = random.randint(10, 100)
+        os_walk.return_value = a2g([rand_n])
+        default_config = self.default_config()
+        harness = Harness(SimpleStreamsCharm)
+        harness.begin()
+        harness.charm._stored.config = default_config
+        harness.charm._get_snapshot_name = Mock()
+        action_event = Mock()
+        harness.charm._on_list_snapshots_action(action_event)
+        assert action_event.set_results.call_args == call({"snapshots":
+                                                           ['snapshot-{}'.format(rand_n)]})
+
+    @patch('shutil.rmtree')
+    def test_delete_snapshot_action(self, shutil_rmtree):
+        default_config = self.default_config()
+        harness = Harness(SimpleStreamsCharm)
+        harness.begin()
+        harness.charm._stored.config = default_config
+        harness.charm._get_snapshot_name = Mock()
+        snapshot_name = uuid4()
+        action_event = Mock(params={"name": snapshot_name})
+        harness.charm._on_delete_snapshot_action(action_event)
+        assert shutil_rmtree.call_args == call('{}/{}'.format(default_config['image-dir'],
+                                                              snapshot_name))
+
+    @patch('os.path.isdir')
+    @patch('os.path.islink')
+    @patch('os.symlink')
+    @patch('os.unlink')
+    def test_publish_snapshot_action_success(self, os_unlink, os_symlink, os_path_islink,
+                                             os_path_isdir):
+        default_config = self.default_config()
+        harness = Harness(SimpleStreamsCharm)
+        harness.begin()
+        harness.charm._stored.config = default_config
+        harness.charm._get_snapshot_name = Mock()
+        snapshot_name = uuid4()
+        action_event = Mock(params={"name": snapshot_name})
+        harness.charm._on_publish_snapshot_action(action_event)
+        assert os_symlink.call_args == call('{}/{}'.format(default_config['image-dir'],
+                                                           snapshot_name),
+                                            '{}/publish'.format(default_config['image-dir']))
+
+    @patch('os.path.isdir')
+    @patch('os.path.islink')
+    @patch('os.symlink')
+    @patch('os.unlink')
+    def test_publish_snapshot_action_fail(self, os_unlink, os_symlink, os_path_islink,
+                                          os_path_isdir):
+        os_path_isdir.return_value = False
+        default_config = self.default_config()
+        harness = Harness(SimpleStreamsCharm)
+        harness.begin()
+        harness.charm._stored.config = default_config
+        harness.charm._get_snapshot_name = Mock()
+        snapshot_name = uuid4()
+        action_event = Mock(params={"name": snapshot_name})
+        harness.charm._on_publish_snapshot_action(action_event)
+        assert action_event.fail.call_args == call('Snapshot does not exist')
