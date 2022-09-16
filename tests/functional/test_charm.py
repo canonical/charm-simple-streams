e5213d637bf45912f34d2619bf87719392dc7c42
Robert Gildein <robert.gildein@canonical.com>
2022-09-17 00:26:20 +0200

    add base function tests and github workflow

---
+++ tests/functional/test_charm.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+
+import pytest
+
+log = logging.getLogger(__name__)
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.skip_if_deployed
+async def test_build_and_deploy(ops_test, series):
+    """Build simple-stream charm and deploy it in bundle."""
+    simple_stream_charm = await ops_test.build_charm(".")
+
+    await ops_test.model.deploy(
+        ops_test.render_bundle(
+            "tests/functional/bundle.yaml",
+            simple_stream_charm=simple_stream_charm,
+            series=series,
+        )
+    )
+    await ops_test.model.wait_for_idle(status="unknown")
+
